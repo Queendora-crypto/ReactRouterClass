82ce5e0d56e6508a9750a3ffbeaf71363d7dab21
Queendora-crypto <onuigbochideragift@gmail.com>
2025-04-29 11:33:34 +0100

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("about-us", "routes/about.tsx"),
-  route("Login", "routes/Login.tsx"),
-  route("Signup", "routes/Signup.tsx"),
+  route("login", "routes/login.tsx"),
+  route("signup", "routes/signup.tsx"),
   route("*", "routes/404.tsx"),
 ] satisfies RouteConfig;
